e19c995ea14a27b6b0412774aa5072bcc3d1c7e3
Lucas Méndez <lucasmendezbaca@gmail.com>
2023-05-12 12:24:40 +0200

    info tablas

--- json2pdf.py
+++ json2pdf.py
@@ -3,6 +3,104 @@ from reportlab.lib.pagesizes import A4
 import json
 from medidas_letras import medidas
 
+##############################################
+with open('AA23060876.json', mode='r') as file:
+    datos = json.load(file)
+
+# c.setFillColor(aColor='black')
+# c.setFont("Helvetica", 7)
+
+
+UNIDADES_MAXIMAS_POR_LINEA = 112
+UNIDADES_MAXIMAS_POR_LINEA_NEGRITA = 160
+
+contador_tablas = 1
+numero_linea = 1
+contador_lineas_cada_tabla = 1
+unidades_en_linea = 0
+texto_liena = ''
+
+# info_por_tabla = [[]]
+# inicializar una lista de listas, donde cada lista tenga 23 elementos
+
+info_tablas = {
+    'codigos': 0,
+    'descripcion_articulos': [[]],
+}
+
+def medida_str(texto):
+    unidades_palabra = 0
+    for letra in texto:
+        unidades_palabra += medidas[letra]
+    return unidades_palabra
+
+for linea in datos['lineas']:
+    for texto in linea[1]:
+        for indice, palabra in enumerate(texto.split(' ')):
+            unidades_palabra = 0
+            if "&.7/rf" in texto:
+                unidades_por_linea = UNIDADES_MAXIMAS_POR_LINEA_NEGRITA
+            else:
+                unidades_por_linea = UNIDADES_MAXIMAS_POR_LINEA
+
+            if "&.7/rf" in palabra:
+                unidades_palabra -= medida_str("&.7/rf>")
+            
+            unidades_palabra += medida_str(palabra)
+
+            if unidades_en_linea + unidades_palabra < unidades_por_linea:
+                unidades_en_linea += unidades_palabra
+                texto_liena += palabra + ' '
+
+                if palabra == texto.split(' ')[-1]:
+                    info_tablas["descripcion_articulos"][-1].append(texto_liena)
+                    numero_linea += 1
+                    contador_lineas_cada_tabla += 1
+                    texto_liena = ''
+                    unidades_en_linea = 0
+                    if contador_lineas_cada_tabla == 23:
+                        info_tablas["descripcion_articulos"].append([])
+                        contador_lineas_cada_tabla = 1
+                        contador_tablas += 1
+
+            else:
+                info_tablas["descripcion_articulos"][-1].append(texto_liena)
+                numero_linea += 1
+                contador_lineas_cada_tabla += 1
+                unidades_en_linea = unidades_palabra
+                texto_liena = palabra + ' '
+                if contador_lineas_cada_tabla == 23:
+                    info_tablas["descripcion_articulos"].append([])
+                    contador_lineas_cada_tabla = 1
+                    contador_tablas += 1
+
+codigos = [['' for _ in range(23)] for _ in range(contador_tablas)]
+
+info_tablas["codigos"] = codigos
+
+for linea in datos['lineas']:
+    codigo = linea[0]
+    if codigo != "" and codigo != " ":
+        indice_lista = 0
+        indice_elemento = 0
+        for indice, lista in enumerate(info_tablas["descripcion_articulos"]):
+            for elemento in lista:
+                if elemento in linea[1][0]:
+                    indice_lista = indice
+                    indice_elemento = lista.index(elemento)
+                    break
+        info_tablas["codigos"][indice_lista][indice_elemento] = codigo
+
+
+print(info_tablas["codigos"])
+
+# print(codigos)
+
+# print(info_tablas["descripcion_articulos"])
+# print(contador_tablas)
+
+##############################################
+
 def writeString(c,x,alto,y,text,size,font='Helvetica-Bold'):
     c.setFont(font,size)
     c.setFillColor(aColor='black')
@@ -127,60 +225,62 @@ for numero_tabla in range(numero):
         writeString(c,490,alto,240,neto,titulo)
         writeString(c,535,alto,240,importes,titulo)
         ##############################################
-        with open('AA23060876.json', mode='r') as file:
-            datos = json.load(file)
+        # with open('AA23060876.json', mode='r') as file:
+        #     datos = json.load(file)
 
-        c.setFillColor(aColor='black')
-        # c.setFont("Helvetica", 7)
-
-
-        UNIDADES_MAXIMAS_POR_LINEA = 112
-        UNIDADES_MAXIMAS_POR_LINEA_NEGRITA = 160
-
-        numero_linea = 1
-        contador_lineas_cada_tabla = 1
-        unidades_en_linea = 0
-        texto_liena = ''
+        # c.setFillColor(aColor='black')
+        # # c.setFont("Helvetica", 7)
 
 
-        for linea in datos['lineas']:
-            for texto in linea[1]:
-                for indice, palabra in enumerate(texto.split(' ')):
-                    unidades_palabra = 0
-                    if "&.7/rf" in texto:
-                        palabra = palabra.replace("&.7/rf>", "")
-                        c.setFont("Helvetica-Bold", 5.4)
-                        unidades_por_linea = UNIDADES_MAXIMAS_POR_LINEA_NEGRITA
-                    else:
-                        c.setFont("Helvetica", 7)
-                        unidades_por_linea = UNIDADES_MAXIMAS_POR_LINEA
+        # UNIDADES_MAXIMAS_POR_LINEA = 112
+        # UNIDADES_MAXIMAS_POR_LINEA_NEGRITA = 160
 
-                    for letra in palabra:
-                        unidades_palabra += medidas[letra]
+        # numero_linea = 1
+        # contador_lineas_cada_tabla = 1
+        # unidades_en_linea = 0
+        # texto_liena = ''
 
-                    if unidades_en_linea + unidades_palabra < unidades_por_linea:
-                        unidades_en_linea += unidades_palabra
-                        texto_liena += palabra + ' '
 
-                        if palabra == texto.split(' ')[-1]:
-                            c.drawString(138, alto - (255 + (numero_linea * 12.8 - 10)), texto_liena)
-                            numero_linea += 1
-                            contador_lineas_cada_tabla += 1
-                            texto_liena = ''
-                            unidades_en_linea = 0
-                    else:
-                        c.drawString(138, alto - (255 + (numero_linea * 12.8 - 10)), texto_liena)
-                        numero_linea += 1
-                        contador_lineas_cada_tabla += 1
-                        unidades_en_linea = unidades_palabra
-                        texto_liena = palabra + ' '
-                        if contador_lineas_cada_tabla  > 23:
-                            break
-                if contador_lineas_cada_tabla > 23:
-                    break
-            if contador_lineas_cada_tabla > 23:
-                contador_lineas_cada_tabla = 1
-                break
+        # for linea in datos['lineas']:
+        #     for codigo in linea[0]:
+        #         pass
+        #     for texto in linea[1]:
+        #         for indice, palabra in enumerate(texto.split(' ')):
+        #             unidades_palabra = 0
+        #             if "&.7/rf" in texto:
+        #                 palabra = palabra.replace("&.7/rf>", "")
+        #                 c.setFont("Helvetica-Bold", 5.4)
+        #                 unidades_por_linea = UNIDADES_MAXIMAS_POR_LINEA_NEGRITA
+        #             else:
+        #                 c.setFont("Helvetica", 7)
+        #                 unidades_por_linea = UNIDADES_MAXIMAS_POR_LINEA
+
+        #             for letra in palabra:
+        #                 unidades_palabra += medidas[letra]
+
+        #             if unidades_en_linea + unidades_palabra < unidades_por_linea:
+        #                 unidades_en_linea += unidades_palabra
+        #                 texto_liena += palabra + ' '
+
+        #                 if palabra == texto.split(' ')[-1]:
+        #                     c.drawString(138, alto - (255 + (numero_linea * 12.8 - 10)), texto_liena)
+        #                     numero_linea += 1
+        #                     contador_lineas_cada_tabla += 1
+        #                     texto_liena = ''
+        #                     unidades_en_linea = 0
+        #             else:
+        #                 c.drawString(138, alto - (255 + (numero_linea * 12.8 - 10)), texto_liena)
+        #                 numero_linea += 1
+        #                 contador_lineas_cada_tabla += 1
+        #                 unidades_en_linea = unidades_palabra
+        #                 texto_liena = palabra + ' '
+        #                 if contador_lineas_cada_tabla  > 23:
+        #                     break
+        #         if contador_lineas_cada_tabla > 23:
+        #             break
+        #     if contador_lineas_cada_tabla > 23:
+        #         contador_lineas_cada_tabla = 1
+        #         break
         ################################################
         if numero_tabla!=numero - 1:
             c.showPage()
